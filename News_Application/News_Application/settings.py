5614442bfed6029778db99874fededf9a9b0703e
Darren1896 <263683028+Darren1896@users.noreply.github.com>
2026-07-02 13:45:03 +0200

    Add files via upload

--- News_Application/News_Application/settings.py
+++ News_Application/News_Application/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f=01_8kcv_fkr)=qj%)_7iw4$+gnykp6lybvg1o^n5_t(ou9b+"
+SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -78,11 +78,11 @@ WSGI_APPLICATION = "News_Application.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "lightfeed_db",
-        "USER": "root",
-        "PASSWORD": "mysqlwork",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "NAME": config('DB_NAME'),
+        "USER": config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD'),
+        "HOST": config('DB_HOST', default='db'),
+        "PORT": config('DB_PORT', default='3306'),
     }
 }
 
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='updates@lightfeed.com')
 
 AUTH_USER_MODEL = "LightFeed.User"
 
